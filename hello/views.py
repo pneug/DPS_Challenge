debfff4cc2e6d4480b082b1e7881d082c94c1cb2
Philip <philngbr@googlemail.com>
2022-05-08 11:37:57 +0200

    Test to receive POST bodies

--- hello/views.py
+++ hello/views.py
@@ -9,11 +9,22 @@ import os
 
 # Create your views here.
 def index(request):
-    times = int(os.environ.get('TIMES', 3))
-    return HttpResponse('Hello! ' * times)
+    # times = int(os.environ.get('TIMES', 3))
+    return HttpResponse("Hello! " + request.body)
+    # return HttpResponse('Hello! ' * 5)
 
 
 def db(request):
+
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, "db.html", {"greetings": greetings})
+
+
+def db_test(request):
 
     greeting = Greeting()
     greeting.save()
